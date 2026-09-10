73806e1bdd56571f9bedee8aa947035e94ab6e58
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-11 00:46:28 +0600

    Added aapi routers

--- app.js
+++ app.js
@@ -4,8 +4,26 @@ require('dotenv').config();
 
 
 const dbConnection = require('./db/connection/connect')
-
 const PORT = process.env.PORT
+
+
+
+
+//  routers
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const orderRouter = require('./routes/orderRoutes');
+
+app.use(express.json())
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/orders', orderRouter);
+
 
 const start = () => {
     try {
