e50dcdc3cedbd9a6152ad68d1e1d7725179ceacb
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-11 00:37:39 +0600

    feat(db): server connected with mongoDB

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express();
 require('dotenv').config();
 
+
+const dbConnection = require('./db/connection/connect')
+
 const PORT = process.env.PORT
 
-app.listen(PORT, () => console.log('server running'));
+const start = () => {
+    try {
+        dbConnection(process.env.MONGO_URI);
+        app.listen(PORT, () => console.log(`Server running at PORT: ${PORT}`))
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+start();
---
+++ db/connection/connect.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"])
+
+const connect = (url) => {
+    return mongoose.connect(url);
+}
+
+module.exports = connect
