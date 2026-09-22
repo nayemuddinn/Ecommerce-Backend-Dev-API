e57bb0ecf2a1a9afaa111dbd6197a701ea6f7bc5
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-22 16:03:37 +0600

    feat(auth); implement password hashing and salting

--- db/models/user.js
+++ db/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -29,5 +30,10 @@ const userSchema = new mongoose.Schema({
     }
 
 });
+
+userSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+})
 
 module.exports = mongoose.model('user', userSchema)
