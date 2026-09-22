f67467a6a95f9c158803300552732b4b5c61519f
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-22 12:27:18 +0600

    feat(auth): add controller method in routing path

---
+++ controllers/authController.js
@@ -0,0 +1,16 @@
+
+
+const register = async (req, res) => {
+    res.status(201).json({ "ok": 1 })
+}
+
+
+const login = async (req, res) => {
+
+}
+
+const logout = async (req, res) => {
+
+}
+
+module.exports = { register, login, logout }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const route = express.Router();
 
+const { register, login, logout } = require('../controllers/authController')
+route.post('/register/', register).post('/login/', login).post('/logout/', logout)
+
+
 
 
 module.exports = route
