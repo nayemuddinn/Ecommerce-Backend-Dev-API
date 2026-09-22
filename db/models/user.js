f471d2efdba102281d28a09150c180a252beccad
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2026-09-22 11:53:02 +0600

    feat(user): add user schema  mongoose model

---
+++ db/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide name'],
+        minlength: 3,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Please provide a valid email'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Pleas Provide password'],
+        minlength: 6
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    }
+
+});
+
+module.exports = mongoose.model('user', userSchema)
